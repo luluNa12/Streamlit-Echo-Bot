9eb036cb9c0455c342272f40f2d8ef514d5fce90
luluNa12 <121836520+luluNa12@users.noreply.github.com>
2025-12-24 00:11:45 -0800

    Update echobot.py

--- echobot.py
+++ echobot.py
@@ -1,8 +1,10 @@
 import streamlit as st
-import numpy as np
 
-with st.chat_message("assistant"):
-    st.write("Hello human")
-    st.bar_chart(np.random.randn(30, 3))
-    st.line_chart(np.random.randn(10, 3))
+with st.chat_message("user"):
+    st.write("Hello 👋")
+
+
+prompt = st.chat_input("Say something")
+if prompt:
+    st.write(f"User has sent the following prompt: {prompt}")
 
